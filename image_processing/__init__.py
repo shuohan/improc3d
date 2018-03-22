8a3994368ab6848956b4bee63a982d6ea15edd92
Shuo Han <shan50@jhu.edu>
2018-03-22 17:01:59 -0400

    Expose padcrop3d

--- image_processing/__init__.py
+++ image_processing/__init__.py
@@ -1,3 +1,4 @@
 from .cropping import crop3d, uncrop3d, calc_bbox3d, resize_bbox3d
 from .deformation import deform3d, calc_random_deformation3d
+from .resizing import padcrop3d
 from .rotation import rotate3d
