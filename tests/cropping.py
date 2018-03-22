16929ad26fc4be10d6c2c4405a8d01511f39cbfe
Shuo Han <shan50@jhu.edu>
2018-03-22 15:39:36 -0400

    Update cropping test to show uncropping result

--- tests/cropping.py
+++ tests/cropping.py
@@ -10,7 +10,7 @@ import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
 
-from image_processing.cropping import crop3d, calc_bbox3d
+from image_processing.cropping import crop3d, calc_bbox3d, uncrop3d
 
 
 parser = argparse.ArgumentParser(description='Test cropping')
@@ -24,26 +24,36 @@ args = parser.parse_args()
 image = nib.load(args.image).get_data()
 mask = nib.load(args.mask).get_data()
 bbox = calc_bbox3d(mask, args.bbox_shape, args.allow_smaller)
-cropped = crop3d(image, bbox)
+cropped, source_bbox, target_bbox = crop3d(image, bbox)
+uncropped = uncrop3d(cropped, image.shape, source_bbox, target_bbox)
 
 plt.figure()
 
 shape = image.shape
 slice_indices = np.array(shape) // 2
-plt.subplot(2, 3, 1)
+plt.subplot(3, 3, 1)
 plt.imshow(image[slice_indices[0], :, :].T)
-plt.subplot(2, 3, 2)
-plt.imshow(image[:, slice_indices[1], :].T)
-plt.subplot(2, 3, 3)
-plt.imshow(image[:, :, slice_indices[2]].T)
+plt.subplot(3, 3, 2)
+plt.imshow(image[:, 150, :].T)
+plt.subplot(3, 3, 3)
+plt.imshow(image[:, :, 40].T)
 
 shape = cropped.shape
 slice_indices = np.array(shape) // 2
-plt.subplot(2, 3, 4)
+plt.subplot(3, 3, 4)
 plt.imshow(cropped[slice_indices[0], :, :].T)
-plt.subplot(2, 3, 5)
+plt.subplot(3, 3, 5)
 plt.imshow(cropped[:, slice_indices[1], :].T)
-plt.subplot(2, 3, 6)
+plt.subplot(3, 3, 6)
 plt.imshow(cropped[:, :, slice_indices[2]].T)
+
+shape = image.shape
+slice_indices = np.array(shape) // 2
+plt.subplot(3, 3, 7)
+plt.imshow(uncropped[slice_indices[0], :, :].T)
+plt.subplot(3, 3, 8)
+plt.imshow(uncropped[:, 150, :].T)
+plt.subplot(3, 3, 9)
+plt.imshow(uncropped[:, :, 40].T)
 
 plt.show()
