1f1ed74da3cc45ff0ef251c4aabb37492b244554
Shuo Han <shan50@jhu.edu>
2018-03-26 16:43:00 -0400

    Add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='image-processing-3d',
+      version='0.2.1',
+      description='Useful functions to process 3D images',
+      author='Shuo Han',
+      author_email='shan50@jhu.edu',
+      packages=['image_processing_3d'])
