4bf35a57c700b1ad7e96c19cb6087331f25eeb39
Shuo Han <shan50@jhu.edu>
2018-03-28 22:22:29 -0400

    Deform multi-channel 3D image

--- tests/deformation.py
+++ tests/deformation.py
@@ -19,28 +19,43 @@ parser.add_argument('-s', '--sigma', default=3, required=False, type=float,
                     help='Control the smoothness of the deformation field')
 parser.add_argument('-l', '--limit', default=3, required=False, type=float,
                     help='The maximum possible deformation in pixel')
+parser.add_argument('-d', '--duplicated-channels', type=int, default=0,
+                    required=False, help='Number of repeats to duplicate the '
+                                         'image to multi-channels')
 args = parser.parse_args()
 
 image = nib.load(args.image).get_data()
+if args.duplicated_channels > 0:
+    image = np.repeat(image[None, ...], args.duplicated_channels, 0)
 
-x_deformation = calc_random_deformation3d(image.shape, args.sigma, args.limit)
-y_deformation = calc_random_deformation3d(image.shape, args.sigma, args.limit)
-z_deformation = calc_random_deformation3d(image.shape, args.sigma, args.limit)
+x_deformation = calc_random_deformation3d(image.shape[-3:], args.sigma,
+                                          args.limit)
+y_deformation = calc_random_deformation3d(image.shape[-3:], args.sigma,
+                                          args.limit)
+z_deformation = calc_random_deformation3d(image.shape[-3:], args.sigma,
+                                          args.limit)
 deformed = deform3d(image, x_deformation, y_deformation, z_deformation)
 
-shape = image.shape
-slice_indices = np.array(shape) // 2
-plt.figure()
-plt.subplot(2, 3, 1)
-plt.imshow(image[slice_indices[0], :, :].T)
-plt.subplot(2, 3, 2)
-plt.imshow(image[:, slice_indices[1], :].T)
-plt.subplot(2, 3, 3)
-plt.imshow(image[:, :, slice_indices[2]].T)
-plt.subplot(2, 3, 4)
-plt.imshow(deformed[slice_indices[0], :, :].T)
-plt.subplot(2, 3, 5)
-plt.imshow(deformed[:, slice_indices[1], :].T)
-plt.subplot(2, 3, 6)
-plt.imshow(deformed[:, :, slice_indices[2]].T)
+if args.duplicated_channels == 0:
+    image = image[None, ...]
+    deformed = deformed[None, ...]
+
+for im, d in zip(image, deformed):
+
+    shape = im.shape
+    slice_indices = np.array(shape) // 2
+    plt.figure()
+    plt.subplot(2, 3, 1)
+    plt.imshow(im[slice_indices[0], :, :].T)
+    plt.subplot(2, 3, 2)
+    plt.imshow(im[:, slice_indices[1], :].T)
+    plt.subplot(2, 3, 3)
+    plt.imshow(im[:, :, slice_indices[2]].T)
+    plt.subplot(2, 3, 4)
+    plt.imshow(d[slice_indices[0], :, :].T)
+    plt.subplot(2, 3, 5)
+    plt.imshow(d[:, slice_indices[1], :].T)
+    plt.subplot(2, 3, 6)
+    plt.imshow(d[:, :, slice_indices[2]].T)
+
 plt.show()
