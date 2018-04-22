56717287810f900f641cded5acca2964445afb17
Shuo Han <shan50@jhu.edu>
2018-04-22 10:13:03 -0400

    Update padcrop3d returns to enable unresize

--- image_processing_3d/resizing.py
+++ image_processing_3d/resizing.py
@@ -14,9 +14,14 @@ def padcrop3d(image, target_shape):
 
     Returns:
         result (3D numpy.array): The resized image
+        source_bbox ((3,) list of slice): The bbox in the source image
+        target_bbox ((3,) list of slice): The bbox in the target image
+
+    NOTE:
+        Use uncrop3d with returned source_bbox and target_bbox to unresize
 
     """
     bbox = [slice(0, s) for s in image.shape]
     resized_bbox = resize_bbox3d(bbox, target_shape, allow_smaller=True)
-    result = crop3d(image, resized_bbox)[0]
-    return result
+    result, source_bbox, target_bbox = crop3d(image, resized_bbox)
+    return result, source_bbox, target_bbox
--- tests/resizing.py
+++ tests/resizing.py
@@ -10,7 +10,7 @@ import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
 
-from image_processing_3d.resizing import padcrop3d
+from image_processing_3d import padcrop3d, uncrop3d
 
 
 parser = argparse.ArgumentParser(description='Test resize')
@@ -19,24 +19,36 @@ parser.add_argument('target_shape', nargs=3, type=int, help='The target shape')
 args = parser.parse_args()
 
 image = nib.load(args.image).get_data()
-resized = padcrop3d(image, args.target_shape)
+resized, source_bbox, target_bbox = padcrop3d(image, args.target_shape)
+unresized = uncrop3d(resized, image.shape, source_bbox, target_bbox)
+diff = image - unresized
 
 shape = image.shape
 slice_indices = np.array(shape) // 2
 plt.figure()
-plt.subplot(2, 3, 1)
+plt.subplot(3, 3, 1)
 plt.imshow(image[slice_indices[0], :, :].T)
-plt.subplot(2, 3, 2)
+plt.subplot(3, 3, 2)
 plt.imshow(image[:, slice_indices[1], :].T)
-plt.subplot(2, 3, 3)
+plt.subplot(3, 3, 3)
 plt.imshow(image[:, :, slice_indices[2]].T)
 
 shape = resized.shape
 slice_indices = np.array(shape) // 2
-plt.subplot(2, 3, 4)
+plt.subplot(3, 3, 4)
 plt.imshow(resized[slice_indices[0], :, :].T)
-plt.subplot(2, 3, 5)
+plt.subplot(3, 3, 5)
 plt.imshow(resized[:, slice_indices[1], :].T)
-plt.subplot(2, 3, 6)
+plt.subplot(3, 3, 6)
 plt.imshow(resized[:, :, slice_indices[2]].T)
+
+shape = diff.shape
+slice_indices = np.array(shape) // 2
+plt.subplot(3, 3, 7)
+plt.imshow(diff[slice_indices[0], :, :].T)
+plt.subplot(3, 3, 8)
+plt.imshow(diff[:, slice_indices[1], :].T)
+plt.subplot(3, 3, 9)
+plt.imshow(diff[:, :, slice_indices[2]].T)
+
 plt.show()
