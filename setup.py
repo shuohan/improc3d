2e02fe4e43b38fa9c210f8fc39d415d03aae4cb6
Shuo Han <shan50@jhu.edu>
2018-05-29 17:44:21 -0400

    Update setup.py for automatic git version

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+from glob import glob
+import subprocess
+
+scripts = glob('scripts/*')
+command = ['git', 'describe', '--tags']
+tag = subprocess.check_output(command).decode().strip()
+head_hash = ['git', 'show-ref', '--head', 'HEAD']
+tag_hash = ['git', 'show-ref', '--tags', tag]
+version = tag if tag_hash == head_hash else head_hash
 
 setup(name='image-processing-3d',
-      version='0.3.0',
+      version=version,
       description='Useful functions to process 3D images',
       author='Shuo Han',
       author_email='shan50@jhu.edu',
